d91347d35ad165c807a4576ac82f42d9347e1511
dongxin <2627405095@qq.com>
2019-09-10 19:24:34 +0800

    add 07

---
+++ 07-module/02-main.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*
+const p = require('./02-export-var.js');
+console.dir(module);
+console.log(p);*/
+/*const circle = require('./02-export-function.js');
+console.log('r = 10, circle area: %d',circle(10).area());
+console.log('r = 10, circle circumference:%d',circle(10).circumference());*/
+
+/*
+const circle = require('./02-export-object.js'),
+      log = console.log;
+log('r = 10,circle diameter:',circle.diameter(10));
+log('r = 10,circle area:',circle.area(10));
+console.dir(module);*/
+
+const  area = require('./02-export-object-v2.js');
+console.log('r = 10,cirle area:',area(10));
+
+
