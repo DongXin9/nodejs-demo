981975bb63fddd51949f78db3aa392919a535c2b
dongxin <2627405095@qq.com>
2019-09-18 15:55:02 +0800

    修改10/02-03-04错误处理

--- 10-file-system/02-my-cp.js
+++ 10-file-system/02-my-cp.js
@@ -2,8 +2,19 @@
 const fs = require('fs'),
       src = process.argv[2],
       dst = process.argv[3];
+var sta,stm;
 //src.pip(dst);
 //src = fs.createReadStream
 //dst = fs.createWriteStream;
-fs.createReadStream(src).pipe(fs.createWriteStream(dst));
+if(fs.existsSync(src)){
+  stm = fs.createReadStream(src).pipe(fs.createWriteStream(dst));
+  //复制x权限
+  //stm.on('close',()=>{
+  //  sta = fs.statSync(src);
+  //  fs.chmodSync(dst,sta.mode);
+  //});
+}else{
+  console.error('%s not exist!',src);
+  process.exit(1);
+}
 
--- 10-file-system/04-my-rm.js
+++ 10-file-system/04-my-rm.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs'),
       file = process.argv[2];
-fs.unlinkSync(file);
+if(fs.existsSync(file)){
+  fs.unlinkSync(file);
+}else{
+  console.error('%s not exist!',file);
+  process.exit(1);
+}
