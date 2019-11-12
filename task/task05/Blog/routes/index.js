683cc641c9bb50a756a73107a08b002302ced398
DongXin9 <2627405095@qq.com>
2019-11-12 16:13:10 +0800

    add task05/Blog

---
+++ task/task05/Blog/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var data = require('../data.json');
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+router.post('/list',function(req,res,next){
+  if(isLegal(req)) {
+    res.render('list',{list:data.chapterList});
+  } else {
+    res.send('用户名密码错误')
+  }
+})
+function isLegal(req) {
+  var isLegal = false;
+  for(var i = 0; i < data.users.length; i++) {
+    if(data.users[i].username === req.body.username && data.users[i].password === req.body.pwd) {
+      isLegal = true;
+      break;
+    }
+  }
+  return isLegal;
+}
+module.exports = router;
